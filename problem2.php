2ba87be8e16da57a5d29350f5569ee10c6735564
Matthew Walker <Walker@Matthews-MacBook-Pro.local>
2020-04-19 15:42:12 -0400

    Problem 2 in PHP

---
+++ problem2.php
@@ -0,0 +1,20 @@
+<?php
+	function readcsv(){
+		$count =0;
+		$file = fopen('address - address.csv', 'r');
+		print_r ("The names are");
+		while (($line = fgetcsv($file)) !== false) {
+  			if($line[3] == "Ontario" && $count!=0){
+  				echo("<br>");
+  				print_r($line[0]);
+  				print_r(" ");
+  				print_r($line[1]);
+  			}
+  			$count++;
+		}
+		fclose($file);
+
+	}
+	readcsv();
+
+?>
